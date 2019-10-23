c762debfada8fc4674802c9e6f216fa4b241bb33
PatriLoto <patricialoto@hotmail.com>
2019-10-23 02:57:35 -0300

    gráfico con theme_dark

--- datosMetereo20_10.R
+++ datosMetereo20_10.R
@@ -416,7 +416,7 @@ x <- c("Día","T.Min.", "T.Media", "T.Máx.")
 y <- sprintf("{point.%s}", c("dia","t_min", "media", "t_max"))
 tltip <- tooltip_table(x, y)
 
-
+# opción 1: theme ffx
 grafico <-hchart(hchartdatos, type = "columnrange",
        hcaes(x = Fecha, low = t_min, high = t_max, color = media)) %>% 
   hc_yAxis(tickPositions = c(-5, 0, 5.0, 10.0,15.0,20.0,25.0,30.0,35.0,40.0),
@@ -430,14 +430,42 @@ grafico <-hchart(hchartdatos, type = "columnrange",
   hc_add_theme(hc_theme_ffx())
  # hc_add_theme(hc_theme_monokai())
 
-#hc_theme_monokai()
-#hc_theme_ffx
-grafico %>% hc_title(text = "Temperaturas máxima y mínima de la ciudad de Corrientes") %>% 
- hc_subtitle(text = "para el período  comprendido entre el 01/01/2011 y el 01/01/2012n \\n Fuente: Claris LPB \\n 
- Los datos fueron obtenidos por la estación meteorológica: Corrientes Aero ubicada según coordenadas: -27.45 (lat) y
--58.76667 (long)")
-            
-    ##\\n Vizualización para DatosdeMiercoles por Patricia Loto utilizando highcharter" )
+grafico %>% hc_title(text = "Temperaturas máxima y mínima de la ciudad de Corrientes <br /> <br /> Para el período  comprendido entre el 01/01/2011 y el 01/01/2012.") %>% 
+ hc_subtitle(text = "La temperatura mínima con -0.4 Cº se registró el 27 de junio, mientras que la máxima con 40.7 Cº se registró el 22 de diciembre. <br /> <br /> Fuente: Claris LPB.")
+     
+#-----------------------------------------------------------------------
+#opción 2: theme monokai
+#-------------------
+#highcharter
+x <- c("Día","T.Min.", "T.Media", "T.Máx.")
+y <- sprintf("{point.%s}", c("dia","t_min", "media", "t_max"))
+tltip <- tooltip_table(x, y)
+
+
+grafico3 <-hchart(hchartdatos, type = "columnrange",
+                 hcaes(x = Fecha, low = t_min, high = t_max, color = media)) %>% 
+  hc_yAxis(tickPositions = c(-5, 0, 5.0, 10.0,15.0,20.0,25.0,30.0,35.0,40.0),
+           gridLineColor = "orange",               #B71C1C
+           labels = list(format = "{value} Cº", useHTML = TRUE)) %>% 
+  hc_tooltip(
+    useHTML = TRUE,
+    headerFormat = as.character(tags$small("{point.x: %Y %b}")),
+    pointFormat = tltip
+  ) %>% 
+  hc_add_theme(hc_theme_flatdark())
+
+grafico3 %>% hc_title(text = "Temperaturas máxima y mínima de la ciudad de Corrientes <br /> <br /> Para el período  comprendido entre el 01/01/2011 y el 01/01/2012.") %>% 
+  hc_subtitle(text = "La temperatura mínima con -0.4 Cº se registró el 27 de junio, mientras que la máxima con 40.7 Cº se registró el 22 de diciembre. <br /> <br /> Fuente: Claris LPB.")
+#hc_theme_flatdark
+#hc_theme_monokai
+
+#--------------------------------------------------------------------------------
+
+
+
+       
+    ##Los datos meteorólogicos de la ciudad de Corrientes Capital son recolectados de manera diaria por la estación meteorológica Corrientes Aero ubicada en la latitud -27.45 y
+#en la longitud -58.76667.<br /> <br />\\n Vizualización para DatosdeMiercoles por Patricia Loto utilizando highcharter" )
 
 #####################################################################
 
